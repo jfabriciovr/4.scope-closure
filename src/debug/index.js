3085e5985626dc13f84b9d6416efe83ae5ae886d
Fabricio Villalobos <fabricio33@gmail.com>
2021-08-26 13:04:52 -0500

    ultimos cambios

---
+++ src/debug/index.js
@@ -0,0 +1,12 @@
+var a = 'Hello';
+
+function hello(){
+    let b = 'soy b';
+    const c = 'soy c';
+    if (true){
+        let d = 'soy d dentro de un if';
+        debugger // hace que se pare el proceso de correr el código en este punto
+    }
+}
+
+hello();
